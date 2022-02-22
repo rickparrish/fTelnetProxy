7a9af5355749fd1ed2204fa344ee73147d23391b
Ree <Ree@PC-HOMESERVER>
2022-02-22 14:05:19 -0500

    Catch and log exceptions

--- Program.cs
+++ Program.cs
@@ -11,64 +11,66 @@ namespace RandM.fTelnetProxy {
         /// The main entry point for the application.
         /// </summary>
         static void Main(string[] args) {
-            // Check for service mode or console mode
-            if (Environment.UserInteractive || OSUtils.IsUnix) {
-                // Console mode, check for arguments
-                if (args.Length > 0) {
-                    try {
-                        // Check entire parameter string for service install or uninstall request
-                        string ParameterString = string.Concat(args).TrimStart('/').TrimStart('-');
-                        switch (ParameterString) {
-                            case "i":
-                            case "install":
-                                Console.WriteLine("Installing service...");
-                                ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
-                                Console.WriteLine("Service installed successfully!");
-                                return;
-                            case "u":
-                            case "uninstall":
-                                Console.WriteLine("Uninstalling service...");
-                                ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
-                                Console.WriteLine("Service uninstalled successfully!");
-                                return;
-                        }
-                    } catch (Exception ex) {
-                        Console.WriteLine("Error handling service request: " + ex.Message);
-                        return;
-                    }
-                }
-
-                // If we get here, we're running as console app
-                using (var fTelnetProxy = new fTelnetProxy()) {
-                    fTelnetProxy.Start();
-
-                    Console.WriteLine("Press A for Active Connections or Q to Quit...");
-
-                    while (true) {
-                        if (Console.KeyAvailable) {
-                            var Ch = Console.ReadKey(true).Key;
-                            if (Ch == ConsoleKey.A) {
-                                fTelnetProxy.DisplayActiveConnections();
-                            } else if (Ch == ConsoleKey.Q) {
-                                break;
-                            } else {
-                                Console.WriteLine(fTelnetProxy.ClientConnectionCount.ToString() + " active connections");
-                                Console.WriteLine("Press A for Active Connections or Q to Quit...");
+            try {
+                // Check for service mode or console mode
+                if (Environment.UserInteractive || OSUtils.IsUnix) {
+                    // Console mode, check for arguments
+                    if (args.Length > 0) {
+                        try {
+                            // Check entire parameter string for service install or uninstall request
+                            string ParameterString = string.Concat(args).TrimStart('/').TrimStart('-');
+                            switch (ParameterString) {
+                                case "install":
+                                    Console.WriteLine("Installing service...");
+                                    ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+                                    Console.WriteLine("Service installed successfully!");
+                                    return;
+                                case "uninstall":
+                                    Console.WriteLine("Uninstalling service...");
+                                    ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
+                                    Console.WriteLine("Service uninstalled successfully!");
+                                    return;
                             }
-                        } else {
-                            Thread.Sleep(100);
+                        } catch (Exception ex) {
+                            Console.WriteLine("Error handling service request: " + ex.Message);
+                            return;
                         }
                     }
 
-                    Console.WriteLine("Exiting...");
+                    // If we get here, we're running as console app
+                    using (var fTelnetProxy = new fTelnetProxy()) {
+                        fTelnetProxy.Start();
 
-                    fTelnetProxy.Stop();
+                        Console.WriteLine("Press A for Active Connections or Q to Quit...");
+
+                        while (true) {
+                            if (Console.KeyAvailable) {
+                                var Ch = Console.ReadKey(true).Key;
+                                if (Ch == ConsoleKey.A) {
+                                    fTelnetProxy.DisplayActiveConnections();
+                                } else if (Ch == ConsoleKey.Q) {
+                                    break;
+                                } else {
+                                    Console.WriteLine(fTelnetProxy.ClientConnectionCount.ToString() + " active connections");
+                                    Console.WriteLine("Press A for Active Connections or Q to Quit...");
+                                }
+                            } else {
+                                Thread.Sleep(1000);
+                            }
+                        }
+
+                        Console.WriteLine("Exiting...");
+
+                        fTelnetProxy.Stop();
+                    }
+                } else {
+                    // Service mode
+                    using (var fTelnetProxyService = new Service()) {
+                        ServiceBase.Run(fTelnetProxyService);
+                    }
                 }
-            } else {
-                // Service mode
-                using (var fTelnetProxyService = new Service()) {
-                    ServiceBase.Run(fTelnetProxyService);
-                }
+            } catch (Exception ex) {
+                RMLog.Exception(ex, "Unhandled exception in main program loop");
             }
         }
     }
