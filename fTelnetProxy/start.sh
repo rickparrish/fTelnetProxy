0393aba6a49ff8535f85bbf86d885ebf3538a960
Ree <Ree@PC-HOMESERVER>
2024-04-14 15:55:58 -0400

    Add start.sh, and remove reference to two other files that were causing build failures

---
+++ fTelnetProxy/start.sh
@@ -0,0 +1,22 @@
+﻿#!/bin/bash
+
+echo "Waiting 5 seconds for network to come online"
+read -t 5 input;
+
+while [ true ]; do
+    # Start fTelnetProxy
+    cd /opt/fTelnetProxy
+    /usr/bin/mono /opt/fTelnetProxy/fTelnetProxy.exe
+
+    # Log the exit code
+    echo "[$(date +"%Y-%m-%d %T")] ExitCode: $?" >> exit_codes.log
+
+    # Wait up to 5 seconds for a keypress to see if admin wants to abort reload
+    echo "----- Press enter to prevent the server from restarting in 5 seconds -----";
+    read -t 5 input;
+    if [ $? == 0 ]; then
+        break;
+    else
+        echo "------------------- SERVER RESTARTS -------------------";
+    fi
+done
